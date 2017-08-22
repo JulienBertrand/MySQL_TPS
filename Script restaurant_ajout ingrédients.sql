99699557e3f6fb8b66b04db9876d1587e59cd861
Julien Bertrand <julien.bertrand6384@gmail.com>
2017-08-22 15:18:20 +0200

    modif

--- Script restaurant_ajout ingrédients.sql
+++ Script restaurant_ajout ingrédients.sql
@@ -1,60 +1,33 @@
 Use ma_base;
 
 INSERT INTO commande (nom, date_du_jour, total)
-	VALUES ('ROBERT', CURDATE(), 152);
-    
-INSERT INTO commande (nom, date_du_jour, total)
-	VALUES ('LEGRAND', CURDATE(), 156);
-    
-INSERT INTO commande (nom, date_du_jour, total)
-	VALUES ('RODRIGUES', CURDATE(), 154);
-    
-INSERT INTO commande (nom, date_du_jour, total)
-	VALUES ('DELPECHE', CURDATE(), 178);
+	VALUES ('ROBERT', CURDATE(), 152),
+           ('LEGRAND', CURDATE(), 156),
+           ('RODRIGUES', CURDATE(), 154),
+           ('DELPECHE', CURDATE(), 178);
     
     
 INSERT INTO Plat (nom, tarif)
-	VALUES ('Entrecote Bordelaise', 32);
-    
-INSERT INTO Plat (nom, tarif)
-	VALUES ('Lieu Noir du Tibet', 56);
-    
-INSERT INTO Plat (nom, tarif)
-	VALUES ('Courgettes Farcies aux Encornets', 35);
-    
-INSERT INTO Plat (nom, tarif)
-	VALUES ('Poulet de Bresse', 32);
+	VALUES ('Entrecote Bordelaise', 32),
+           ('Lieu Noir du Tibet', 56),
+           ('Courgettes Farcies aux Encornets', 35),
+           ('Poulet de Bresse', 32),
+           ('Gnoccis',12);
     
     
 INSERT INTO Ingredient (nom)
-	VALUES ('tomate');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('fromage');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('Poulet');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('entrecote');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('Lieu');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('Courgette');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('Encornets');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('Creme');
-    
-INSERT INTO Ingredient (nom)
-	VALUES ('Vin');
+	VALUES ('tomate'),
+           ('fromage'),
+           ('Poulet'), 
+           ('entrecote'), 
+           ('Lieu'), 
+           ('Courgette'), 
+           ('Encornets'),
+		   ('Creme'),
+		   ('Vin');
     
 INSERT INTO commande_plats (ID_C_commande_plats, ID_P_commande_plats)
-    VALUES (1,1),  -- la Famille Robert a commandé une entrecote Bordelaise.
+    VALUES (1,1),  -- la Famille Robert a commandé une entrecote Bordelaise.plat_ingredient
            (1,4),  -- La famille Robert a également commandé un poulet de Bresse.
 		   (2,2),  -- La famille Legrand a commandé un Lieu Noir.
            (2,2),  -- Et finalement en a commandé un second.
